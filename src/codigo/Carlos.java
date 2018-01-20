122120b198c124f98fb83a21c500eb7fc9748348
carlo <carlo@192.168.1.26>
2018-01-20 14:09:07 +0100

    Subido ejercicio en la clase Carlos.

--- src/codigo/Carlos.java
+++ src/codigo/Carlos.java
@@ -10,5 +10,32 @@ package codigo;
  * @author carlosabia
  */
 public class Carlos {
+    public void run(){
+        roundSum(16, 17, 18);
+        roundSum(12, 13, 14);
+        roundSum(6, 4, 4);
+}
     
+    
+    
+    
+    
+    
+    
+    
+    
+   public int roundSum(int a, int b, int c) {
+  return round10(a) + round10(b) + round10(c);
+}
+
+public int round10(int num) {
+    int single = num % 10;
+    if(single >= 5){
+    num += (10 - single);
+    }
+    else{
+    num -= single;
+    }
+  return num;
+} 
 }
